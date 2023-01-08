3a60bf3d3e149291bdc5bc8874e0affd3b22507c
chardos <chardos@gmail.com>
2023-01-08 12:45:37 +1100

    box comin at ya

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,7 +1,16 @@
+import { useFrame } from "@react-three/fiber";
+import { useState } from "react";
+
 function Scene({}) {
+  const [zPos, setZPos] = useState(-30);
+  useFrame((state, delta, xrFrame) => {
+    setZPos(zPos + 0.3);
+    // This function runs at the native refresh rate inside of a shared render-loop
+  });
+
   return (
     <>
-      <mesh>
+      <mesh position={[0, 0, zPos]}>
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color={"orange"} />
       </mesh>
